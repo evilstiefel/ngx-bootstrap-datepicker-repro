879b5ad9bacaf3390e0bdeeacaec1753573f0912
Steffen Sachse <steffen.sachse@gecko.de>
2021-08-11 15:02:50 +0200

    Minimal reproduction added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'datepicker-disabled-repro';
+  firstDate = new Date();
+
+  datesDisabled: Date[] = [];
+
+  public counter = 0;
+
+  disableDates() {
+    this.datesDisabled = [this.firstDate];
+  }
+
+  enableDates() {
+    this.datesDisabled = [];
+  }
+
+  increaseCounter() {
+    this.counter++;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { DatepickerModule } from 'ngx-bootstrap/datepicker';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 @NgModule({
   declarations: [
@@ -12,7 +12,7 @@ import { DatepickerModule } from 'ngx-bootstrap/datepicker';
   ],
   imports: [
     BrowserModule,
-    DatepickerModule.forRoot(),
+    BsDatepickerModule.forRoot(),
     BrowserAnimationsModule
   ],
   providers: [],
